8de3f488aac8b5f85bb9d7f980886aee354d7a6d
Sergio Tendero <stendero@gmail.com>
2019-01-02 21:39:50 +0100

    Add Routing / Navigation (Dashboard component)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,15 +8,8 @@ import {HeroService} from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes: Hero[];
 
-  heroes : Hero [];
-
-  selectedHero: Hero;
-
-  onSelect(hero : Hero): void {
-    this.selectedHero = hero;
-  }
-  
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
@@ -24,7 +17,8 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
-
 }
+
